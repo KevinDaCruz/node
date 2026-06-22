7281ba9bdc8cfc8565286cff92f44e7b3f82cbbb
KevinDaCruz <kevindc39@hotmail.com>
2026-06-22 14:34:41 +0200

    Activité 7

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
 require("dotenv").config();
-const express = require("express");
-const app = express();
+const app = require("./app.js");
 const port = process.env.PORT;
-
-console.log("PORT chargé :", port);
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
