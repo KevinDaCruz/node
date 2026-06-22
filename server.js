8f27cb7ad96e269d2a9d68052794a40606ec2f1a
KevinDaCruz <kevindc39@hotmail.com>
2026-06-22 13:26:06 +0200

    Activité 5

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT;
 
+console.log("PORT chargé :", port);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
