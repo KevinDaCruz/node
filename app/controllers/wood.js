d32158a18e74d8be869aaa4e617214ead334f894
KevinDaCruz <kevindc39@hotmail.com>
2026-06-23 12:31:49 +0200

    Activité 14 : Corrections

--- app/controllers/wood.js
+++ app/controllers/wood.js
@@ -1,10 +1,14 @@
 import { prisma } from "../../app.js";
 
-export const listWoods = async (req, res) => {
+export const readAll = async (req, res) => {
   try {
     const woods = await prisma.wood.findMany();
     res.status(200).json(woods);
   } catch (error) {
-    res.status(500).json({ error: error.message || error });
+    res
+      .status(500)
+      .json({
+        error: error?.message || "An error occurred while fetching woods",
+      });
   }
 };
