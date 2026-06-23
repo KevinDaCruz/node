e89e51e624f68ce719ef99fa24eb512fcee904dc
KevinDaCruz <kevindc39@hotmail.com>
2026-06-23 13:01:51 +0200

    Activité 15

--- app/controllers/wood.js
+++ app/controllers/wood.js
@@ -5,10 +5,24 @@ export const readAll = async (req, res) => {
     const woods = await prisma.wood.findMany();
     res.status(200).json(woods);
   } catch (error) {
-    res
-      .status(500)
-      .json({
-        error: error?.message || "An error occurred while fetching woods",
-      });
+    res.status(500).json({
+      error: error?.message || "An error occurred while fetching woods",
+    });
+  }
+};
+
+export const readByHardness = async (req, res) => {
+  try {
+    const woods = await prisma.wood.findMany({
+      where: {
+        hardness: req.params.hardness,
+      },
+    });
+    res.status(200).json(woods);
+  } catch (error) {
+    res.status(500).json({
+      error:
+        error?.message || "An error occurred while fetching woods by hardness",
+    });
   }
 };
