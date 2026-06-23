8740399f69728d657e58eefa8eed605319ea1892
KevinDaCruz <kevindc39@hotmail.com>
2026-06-23 12:21:05 +0200

    Activité 14

--- app/controllers/wood.js
+++ app/controllers/wood.js
@@ -1,3 +1,10 @@
-export const listWoods = (req, res) => {
-  res.send("List of woods");
+import { prisma } from "../../app.js";
+
+export const listWoods = async (req, res) => {
+  try {
+    const woods = await prisma.wood.findMany();
+    res.status(200).json(woods);
+  } catch (error) {
+    res.status(500).json({ error: error.message || error });
+  }
 };
