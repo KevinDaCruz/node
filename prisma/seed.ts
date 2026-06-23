7060821d2f767d57dd422d09e5c03492bf0bb796
KevinDaCruz <kevindc39@hotmail.com>
2026-06-23 09:43:47 +0200

    Activité 8 : Corrections

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,10 @@
 import { prisma } from "../app.js";
 
 async function main() {
-  await prisma.user.create({
-    data: {
+  await prisma.user.upsert({
+    where: { email: "john.doe@example.com" },
+    update: {},
+    create: {
       firstName: "John",
       lastName: "Doe",
       email: "john.doe@example.com",
@@ -10,17 +12,23 @@ async function main() {
     },
   });
 
-  await prisma.wood.createMany({
-    data: [
-      { name: "Épicéa", type: "softwood", hardness: "tender" },
-      { name: "Pin", type: "softwood", hardness: "medium_hard" },
-      { name: "Padouk", type: "exotic_wood", hardness: "hard" },
-      { name: "Érable", type: "noble_and_hardwoods", hardness: "medium_hard" },
-      { name: "Hêtre", type: "noble_and_hardwoods", hardness: "medium_hard" },
-      { name: "Itauba", type: "exotic_wood", hardness: "hard" },
-      { name: "Douglas", type: "softwood", hardness: "tender" },
-    ],
-  });
+  const woods = [
+    { name: "Épicéa", type: "softwood", hardness: "tender" },
+    { name: "Pin", type: "softwood", hardness: "medium_hard" },
+    { name: "Padouk", type: "exotic_wood", hardness: "hard" },
+    { name: "Érable", type: "noble_and_hardwoods", hardness: "medium_hard" },
+    { name: "Hêtre", type: "noble_and_hardwoods", hardness: "medium_hard" },
+    { name: "Itauba", type: "exotic_wood", hardness: "hard" },
+    { name: "Douglas", type: "softwood", hardness: "tender" },
+  ];
+
+  for (const wood of woods) {
+    await prisma.wood.upsert({
+      where: { name: wood.name },
+      update: {},
+      create: wood,
+    });
+  }
 
   console.log("Seed terminé !");
 }
